677f885b5e6d310b86e99266772bdbec65644805
Thenemex <tnmx777@gmail.com>
2026-03-07 18:49:42 +0100

    Fully modernized the config setup

--- src/main/java/com/harvestingxp/Config.java
+++ src/main/java/com/harvestingxp/Config.java
@@ -1,23 +1,67 @@
 package com.harvestingxp;
 
 import net.minecraftforge.common.ForgeConfigSpec;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.config.ModConfigEvent;
 
 @Mod.EventBusSubscriber(modid = HarvestingCropsXP.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class Config {
 
-    private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+    public static final ForgeConfigSpec SPEC;
+    public static final Values VALUES;
 
-    protected static final ForgeConfigSpec.IntValue XP = BUILDER.comment("The amount of XP given to the player when he harvest crops").defineInRange("xp", 5, 0, 100);
+    public static class Values {
+        private final ForgeConfigSpec.BooleanValue GIVE_XP_TO_PLAYER_ENABLED, SPAWN_XP_ORB_ENABLED;
+        private final ForgeConfigSpec.IntValue XP_GIVEN_TO_PLAYER_AMOUNT, XP_ORB_VALUE_AMOUNT;
 
-    static final ForgeConfigSpec SPEC = BUILDER.build();
+        private Values(ForgeConfigSpec.Builder builder) {
+            builder.push("Give XP to Player");
+            {
+                GIVE_XP_TO_PLAYER_ENABLED = builder
+                        .comment(" Will give the xp directly to player harvesting the crops")
+                        .define("giveXPtoPlayer_enabled", true);
 
-    public static int xp;
+                XP_GIVEN_TO_PLAYER_AMOUNT = builder
+                        .comment(" The amount of XP the player will receive")
+                        .defineInRange("xpGiven_value", 5, 0, 100);
 
-    @SubscribeEvent
-    static void onLoad(final ModConfigEvent event) {
-        xp = XP.get();
+                builder.pop();
+            }
+
+            builder.push("Spawning XP Orbs");
+            {
+                SPAWN_XP_ORB_ENABLED = builder
+                        .comment(" Will spawn an XP Orb on the crop block harvested by the player")
+                        .define("spawnXPOrb_enabled", false);
+
+                XP_ORB_VALUE_AMOUNT = builder
+                        .comment(" The amount of XP contained in the orb")
+                        .defineInRange("xpOrb_value", 5, 0, 100);
+
+                builder.pop();
+            }
+        }
     }
+
+    static {
+        var pair = new ForgeConfigSpec.Builder().configure(Values::new);
+        VALUES = pair.getLeft();
+        SPEC = pair.getRight();
+    }
+
+    // Give XP to Player
+    public static boolean isGiveXpToPlayerEnabled() {
+        return VALUES.GIVE_XP_TO_PLAYER_ENABLED.get();
+    }
+    public static int getXpGivenValue() {
+        return VALUES.XP_GIVEN_TO_PLAYER_AMOUNT.get();
+    }
+    // Spawning XP Orbs
+    public static boolean isSpawnXpOrbEnabled() {
+        return VALUES.SPAWN_XP_ORB_ENABLED.get();
+    }
+    public static int getXpOrbValue() {
+        return VALUES.XP_ORB_VALUE_AMOUNT.get();
+    }
+
+
 }
